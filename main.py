551b34de638ecfb8fd30b73eca0429da3b51af99
iamsadat <sadatullahkhan40@gmail.com>
2023-09-17 02:40:33 +0530

    Set up Virtual Environment and Twilio

---
+++ main.py
@@ -0,0 +1,20 @@
+from twilio.rest import Client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+account_sid = os.getenv("account_sid")
+auth_token = os.getenv("auth_token")
+print(f"account_sid: {account_sid}")
+print(f"auth_token: {auth_token}")
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    from_="whatsapp:+14155238886",
+    body="Hello from the bot!",
+    to="whatsapp:+919989680670",
+)
+
+print(message.sid)
